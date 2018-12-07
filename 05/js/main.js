61a943db1ec473d86d0c2733e3b152fa571f8c0e
Carlos Mañas <carlos@spacenomads.com>
2018-12-07 02:42:10 +0100

    Add solution number 5 (#4)

---
+++ 05/js/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const puzzleData = 'js/puzzle.json';
+const testPuzzle = 'dabAcCaCBAcCcaDA';
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const reactionCases = new RegExp(createCasesList(alphabet).join('|'),'g');
+const unitCases = createUnitsList(alphabet);
+const result1 = document.querySelector('#result-1');
+const result2 = document.querySelector('#result-2');
+
+
+function duplicateMe(str) {
+  return str+str;
+}
+
+function createCasesList(str) {
+  const result = [];
+  for (const r of str.split('')) {
+    const i = [r.toLowerCase() + r.toUpperCase(), r.toUpperCase() + r.toLowerCase()];
+    result.push(i);
+  }
+  return result.flat();
+}
+
+function createUnitsList(str) {
+  const result = [];
+  for (const r of str.split('')) {
+    const i = [r.toLowerCase() + '|' +r.toUpperCase()];
+    result.push(i);
+  }
+  return result.flat();
+}
+
+function activateReaction(polymer) {
+  let newPolymer = polymer;
+  if (polymer.match(reactionCases)) {
+    newPolymer = polymer.replace(reactionCases, '');
+    return activateReaction(newPolymer);
+  } else {
+    return newPolymer.length;
+  }
+}
+
+function getLowerCount(arr) {
+  arr.sort((a,b)=> a.count - b.count);
+  return arr[0].count;
+}
+
+function getUnitActivationResult(str) {
+  let results = [];
+  for (const unit of unitCases) {
+    const test = str.replace( new RegExp(unit, 'g'), '' );
+    results.push({unit: unit[0].toLowerCase(), count: activateReaction(test)});
+  }
+  return getLowerCount(results);
+}
+
+fetch(puzzleData)
+  .then(res => res.json())
+  .then(data => {
+
+    // Part 1 
+    result1.innerHTML = activateReaction(data.polymer);
+    
+    // Part 2
+    result2.innerHTML = getUnitActivationResult(data.polymer);
+  });
+
