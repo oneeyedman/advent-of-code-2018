0d125d187ac25f26f166c55265b7bb2fb3da44bd
Carlos Mañas <carlos@spacenomads.com>
2018-12-24 00:22:57 +0100

    I don't now how to continue this :(

--- 07/js/main.js
+++ 07/js/main.js
@@ -4,6 +4,11 @@ const puzzleData = 'js/puzzle.json';
 const result1 = document.querySelector('#result-1');
 const result2 = document.querySelector('#result-2');
 
+let startPoint;
+let endPoint;
+let paths;
+let path;
+
 const testPuzzle = [
   "Step C must be finished before step A can begin.",
   "Step C must be finished before step F can begin.",
@@ -23,55 +28,85 @@ function extractRules(arr) {
   return result;
 }
 
-function getUniqueSteps(arr) {
+function getUniqueItems(arr) {
   const u = new Set(arr.flat());
   return Array.from(u);
 }
 
-const rules = extractRules(testPuzzle);
-const steps = getUniqueSteps(rules);
-console.log(steps);
-
-
-steps.sort((a,b)=>{
-  if (a==='C' && b == 'A') {
-    return -1;
-  } else if (a==='C' && b == 'F') {
-    return -1;
-  } else if (a==='C' && b == 'F') {
-    return -1;
-  } else if (a==='A' && b == 'B') {
-    return -1;
-  } else if (a==='A' && b == 'D') {
-    return -1;
-  } else if (a==='B' && b == 'E') {
-    return -1;
-  } else if (a==='D' && b == 'E') {
-    return -1;
-  } else if (a==='F' && b == 'E') {
-    return -1;
-  } else {
-    return 0;
-  }
-});
-console.log('---');
-console.log(steps);
-
-console.log(
-  '%cHave a nice day!',
-  'font-size: 20px; background-color: yellow; color:red; margin-left: 20px;'
-);
-
-for (let i=0;i<10;i++) {
-  console.log(i);
+function getNotIncludedLetter(arr1,arr2) {
+  return arr1.filter((item, index)=>{
+    if (!arr2.includes(item))
+      return item;
+  });
 }
 
+function getStartPoint(arr) {
+  let zeroLetters = getUniqueItems(arr.map(i=>i[0]));
+  let unoLetters = getUniqueItems(arr.map(i=>i[1]));
+  let startLetter = getNotIncludedLetter(zeroLetters, unoLetters);
+
+  return startLetter.toString();
+}
+
+function getEndPoint(arr) {
+  let zeroLetters = getUniqueItems(arr.map(i=>i[0]));
+  let unoLetters = getUniqueItems(arr.map(i=>i[1]));
+  let endLetter = getNotIncludedLetter(unoLetters, zeroLetters);
+
+  return endLetter.toString();
+}
+
+function getTotalPaths(letter,arr) {
+  return arr.filter(item=>item[1]===letter).length;
+}
+
+function getNextStep(letter, steps) {
+  return steps.filter(i=>i[0]===letter).map(i=>i[1]);
+}
+
+function getPaths(letter,steps, acc) {
+  if (letter === startPoint) {
+    acc.push(letter);
+  }
+  const n = getNextStep(letter, steps);
+  console.log(n);
+  if (n.length > 1) {
+    for (const i of n) {
+      console.log(i);
+      acc.push(i);
+      getPaths(i,steps, acc);
+
+    }
+  } else if (n[0] === endPoint) {
+    console.log('fin');
+    acc.push(n[0]);
+    return;
+  } else {
+    acc.push(n[0]);
+  }
+}
+
+
+// ---
 
 fetch(puzzleData)
   .then(res => res.json())
   .then(data => {
 
     // Part 1
+      const rules = extractRules(testPuzzle);
+      console.log(rules);
+      startPoint = getStartPoint(rules);
+      endPoint = getEndPoint(rules);
+      paths = getTotalPaths(endPoint,rules);
+      path = [];
+      console.log('>', startPoint);
+      console.log('>', endPoint);
+      console.log('>', paths);
+
+
+      getPaths(startPoint, rules, path);
+      console.log('>>', path);
     //result1.innerHTML = ;
 
     // Part 2
